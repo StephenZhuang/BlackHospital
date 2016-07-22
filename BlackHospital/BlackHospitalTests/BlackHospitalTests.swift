2cff99907c3e4f84d400c1c3bc5b275b2581d467
StephenZhuang <StephenZhuang@longgedeMBA.local>
2016-07-22 12:11:48 +0800

    Cteate Project

---
+++ BlackHospital/BlackHospitalTests/BlackHospitalTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlackHospitalTests.swift
+//  BlackHospitalTests
+//
+//  Created by Stephen Zhuang on 16/7/22.
+//  Copyright © 2016年 StephenZhuang. All rights reserved.
+//
+
+import XCTest
+@testable import BlackHospital
+
+class BlackHospitalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
