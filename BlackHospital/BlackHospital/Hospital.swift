8a47503453c5e63e32d084ddd7cec50ea41c7b09
StephenZhuang <StephenZhuang@longgedeMBA.local>
2016-07-28 12:08:21 +0800

    realm

---
+++ BlackHospital/BlackHospital/Hospital.swift
@@ -0,0 +1,19 @@
+//
+//  Hospital.swift
+//  BlackHospital
+//
+//  Created by Stephen Zhuang on 16/7/28.
+//  Copyright © 2016年 StephenZhuang. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Hospital: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
